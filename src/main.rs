310538aa776334244845570a782f086b97291af0
Eugene Lebedev <duke.tougu@gmail.com>
2019-09-20 15:08:15 +0300

    [!] fix: include-custom-domains option didn't work properly

--- src/main.rs
+++ src/main.rs
@@ -59,8 +59,9 @@ fn main() {
             }
         } else {
 
-            if include_custom_domains && nginx_vhost.port != DEFAULT_HTTPS_PORT {
-                if !vector_contains_same_domain_with_default_http_port(&vhosts, &nginx_vhost.domain) {
+            if nginx_vhost.port != DEFAULT_HTTPS_PORT {
+                if include_custom_domains &&
+                   !vector_contains_same_domain_with_default_http_port(&vhosts, &nginx_vhost.domain) {
                     vhosts.push(nginx_vhost)
                 }
 
@@ -78,8 +79,9 @@ fn main() {
                 vhosts.push(apache_vhost)
             }
         } else {
-            if include_custom_domains && apache_vhost.port != DEFAULT_HTTPS_PORT {
-                if !vector_contains_same_domain_with_default_http_port(&vhosts, &apache_vhost.domain) {
+            if apache_vhost.port != DEFAULT_HTTPS_PORT {
+                if include_custom_domains &&
+                   !vector_contains_same_domain_with_default_http_port(&vhosts, &apache_vhost.domain) {
                     vhosts.push(apache_vhost)
                 }
 
