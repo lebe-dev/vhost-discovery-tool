573c5cb10ad222ad32ef0814bd92f1b61781e094
Eugene Lebedev <duke.tougu@gmail.com>
2019-09-20 13:58:35 +0300

    [+] add tests

--- src/main_tests.rs
+++ src/main_tests.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod main_tests {
-    use crate::{DEFAULT_HTTP_PORT, DEFAULT_HTTPS_PORT, get_site_name};
+    use crate::{DEFAULT_HTTP_PORT, DEFAULT_HTTPS_PORT, get_site_name, get_url};
+
+    const CUSTOM_VHOST_PORT: i32 = 5382;
 
     #[test]
     fn get_site_name_with_standard_port_should_return_name_without_port() {
@@ -13,10 +15,32 @@ mod main_tests {
     #[test]
     fn get_site_name_with_nonstandard_port_should_return_name_with_port() {
         let domain = "sub.diggers.ru";
-        let custom_port = 5382;
+        let expected_domain = format!("{}:{}", domain, CUSTOM_VHOST_PORT);
 
-        let expected_domain = format!("{}:{}", domain, custom_port);
+        assert_eq!(get_site_name(domain, CUSTOM_VHOST_PORT), expected_domain);
+    }
 
-        assert_eq!(get_site_name(domain, custom_port), expected_domain);
+    #[test]
+    fn get_url_should_return_url_with_https_for_443_port() {
+        let domain = "quarkoman.com";
+        let expected_url = format!("https://{}", domain);
+
+        assert_eq!(get_url(domain, DEFAULT_HTTPS_PORT), expected_url)
+    }
+
+    #[test]
+    fn get_url_should_return_url_without_port_for_default_http_port() {
+        let domain = "quarkoman.com";
+        let expected_url = format!("http://{}", domain);
+
+        assert_eq!(get_url(domain, DEFAULT_HTTP_PORT), expected_url)
+    }
+
+    #[test]
+    fn get_url_should_return_url_with_port_when_custom_port_provided() {
+        let domain = "quarkoman.com";
+        let expected_url = format!("http://{}:{}", domain, CUSTOM_VHOST_PORT);
+
+        assert_eq!(get_url(domain, CUSTOM_VHOST_PORT), expected_url)
     }
 }
