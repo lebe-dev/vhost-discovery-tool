3a2a1563ca28d1206de0924fb4c1bf2e43900fec
Eugene Lebedev <duke.tougu@gmail.com>
2020-03-02 13:39:19 +0300

    [+] bump app version to 1.0.1

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn main() {
     log4rs::init_config(logging_config).unwrap();
 
     let matches = App::new("Site Discovery Flea")
-                                    .version("1.0.0")
+                                    .version("1.0.1")
                                     .author("Eugene Lebedev <duke.tougu@gmail.com>")
                                     .about("Discover site configs for nginx and apache. \
                                             Then generate urls and show output in \
