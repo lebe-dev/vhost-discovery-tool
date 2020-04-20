6ba33094869d6c57352be92586cac52468b898c7
Eugene Lebedev <duke.tougu@gmail.com>
2020-04-20 12:46:28 +0300

    [~] write log file to /var/log/site-discovery-flea.log

--- src/logging.rs
+++ src/logging.rs
@@ -1,6 +1,4 @@
 pub mod logging {
-    use std::path::Path;
-
     use log4rs::append::file::FileAppender;
     use log4rs::config::{Appender, Config, Logger, Root};
     use log4rs::encode::pattern::PatternEncoder;
@@ -8,9 +6,9 @@ pub mod logging {
 
     const FILE_APPENDER_NAME: &str = "file";
 
-    pub fn get_logging_config(work_dir: &str) -> Config {
+    pub fn get_logging_config(log_file_path: &str) -> Config {
         Config::builder()
-            .appender(get_file_appender_definition(work_dir))
+            .appender(get_file_appender_definition(log_file_path))
             .logger(get_default_logger())
             .build(
             Root::builder()
@@ -20,16 +18,13 @@ pub mod logging {
             .unwrap()
     }
 
-    fn get_file_appender_definition(work_dir: &str) -> Appender {
+    fn get_file_appender_definition(log_file_path: &str) -> Appender {
         Appender::builder()
-            .build(FILE_APPENDER_NAME, Box::new(get_file_appender(work_dir))
+            .build(FILE_APPENDER_NAME, Box::new(get_file_appender(log_file_path))
         )
     }
 
-    fn get_file_appender(work_dir: &str) -> FileAppender {
-        let log_file_path = Path::new(work_dir).join("site-discovery-flea.log")
-                                                          .display().to_string();
-
+    fn get_file_appender(log_file_path: &str) -> FileAppender {
         FileAppender::builder()
             .encoder(get_encoder())
             .build(log_file_path)
