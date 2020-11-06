3aa5ebffe680e4e43e9825d743c6484ccbcd0a2e
Eugene Lebedev <duke.tougu@gmail.com>
2020-11-06 22:26:30 +0300

    [~] refactoring

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,15 @@ extern crate serde_json;
 
 use std::env;
 use std::path::Path;
-use std::process::exit;
 
 use clap::{App, Arg, ArgMatches};
 use serde::Serialize;
 use serde_json::json;
 
-use crate::apache::apache::{get_apache_redirect_to_http_regex, get_apache_vhost_port_regex, get_domain_search_regex_for_apache_vhost};
+use crate::apache::apache::get_apache_vhosts;
 use crate::domain::domain::VirtualHost;
 use crate::logging::logging::get_logging_config;
 use crate::nginx::nginx::get_nginx_vhosts;
-use crate::webserver::webserver::{get_vhost_config_file_list, get_virtual_hosts_from_file};
 
 mod logging;
 
@@ -243,51 +241,6 @@ fn get_site_name(domain: &str, port: i32) -> String {
     } else {
         String::from(format!("{}:{}", domain, port))
     }
-}
-
-fn get_apache_vhosts(vhosts_path: &Path) -> Vec<VirtualHost> {
-    debug!("get virtual hosts from apache configs");
-    debug!("configs path '{}'", vhosts_path.display());
-
-    let mut vhosts: Vec<VirtualHost> = Vec::new();
-
-    if vhosts_path.is_dir() && vhosts_path.exists() {
-        match get_vhost_config_file_list(vhosts_path) {
-            Ok(vhost_files) => {
-                for vhost_file in vhost_files {
-                    let vhost_file_path = vhost_file.as_path();
-
-                    debug!("analyze vhost file '{}'", vhost_file_path.display());
-
-                    let section_start_regex = get_apache_vhost_port_regex();
-                    let redirect_to_url_regex = get_apache_redirect_to_http_regex();
-                    let port_search_regex = get_apache_vhost_port_regex();
-                    let domain_search_regex = get_domain_search_regex_for_apache_vhost();
-
-                    if let Ok(apache_vhosts) = get_virtual_hosts_from_file(
-                        vhost_file_path,
-                        section_start_regex,
-                        redirect_to_url_regex,
-                        port_search_regex,
-                        domain_search_regex,
-                    ) {
-                        for apache_vhost in apache_vhosts {
-                            debug!("{}", apache_vhost.to_string());
-                            vhosts.push(apache_vhost);
-                        }
-
-                    } else { error!("unable to get virtual hosts from file") }
-                }
-            }
-            Err(_) => {
-                error!("unable to get vhost file list from '{}', \
-                        possible reason: lack of permissions", vhosts_path.display());
-                exit(ERROR_EXIT_CODE)
-            }
-        }
-    }
-
-    return vhosts;
 }
 
 fn get_url(domain: &str, vhost_port: i32) -> String {
