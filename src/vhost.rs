39cafcee1594a8e492838cdd090814a1535b0b43
Eugene Lebedev <duke.tougu@gmail.com>
2022-07-17 20:19:28 +0300

    Add documentation for VhostDiscoveryConfig

--- src/vhost.rs
+++ src/vhost.rs
@@ -1,9 +1,24 @@
 use regex::Regex;
 
 pub struct VhostDiscoveryConfig {
+    /// Pattern for vhost section start
+    ///
+    /// Example for nginx:
+    ///
+    /// ```nginx
+    /// server {
+    /// ```
     pub section_start: Regex,
+
+    /// Pattern for redirect to another url
     pub redirect_to_url: Regex,
+
+    /// Pattern for vhost port
     pub port: Regex,
+
+    /// Pattern for vhost domain
     pub domain: Regex,
+
+    /// Scan sub-directories for vhost files
     pub include_subdirs: bool
 }
